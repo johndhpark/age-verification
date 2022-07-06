b96aadea5b0ceaee01cafbcdd14e3a7b4ffcfa02
John DH. Park <john.dh.park@me.com>
2022-07-06 09:44:02 -0700

    Code Cleanup

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -5,60 +5,60 @@ import Card from "./Card";
 import classes from "./ErrorModal.module.css";
 
 const Backdrop = ({ onConfirm }) => (
-	/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */
-	<div
-		className={classes.backdrop}
-		onClick={onConfirm}
-		onKeyPress={onConfirm}
-	/>
+  /* eslint-disable-next-line jsx-a11y/no-static-element-interactions */
+  <div
+    className={classes.backdrop}
+    onClick={onConfirm}
+    onKeyPress={onConfirm}
+  />
 );
 
 const ModalOverlay = ({ title, message, onConfirm }) => (
-	<Card className={classes.modal}>
-		<header className={classes.header}>
-			<h2>{title}</h2>
-		</header>
-		<div className={classes.content}>
-			<p>{message}</p>
-		</div>
-		<footer className={classes.actions}>
-			<Button onClick={onConfirm}>Okay</Button>
-		</footer>
-	</Card>
+  <Card className={classes.modal}>
+    <header className={classes.header}>
+      <h2>{title}</h2>
+    </header>
+    <div className={classes.content}>
+      <p>{message}</p>
+    </div>
+    <footer className={classes.actions}>
+      <Button onClick={onConfirm}>Okay</Button>
+    </footer>
+  </Card>
 );
 
 const ErrorModal = ({ title, message, onConfirm }) => (
-	<>
-		{ReactDOM.createPortal(
-			<Backdrop onClick={onConfirm} />,
-			document.getElementById("backdrop-root")
-		)}
-		{ReactDOM.createPortal(
-			<ModalOverlay title={title} message={message} onConfirm={onConfirm} />,
-			document.getElementById("overlay-root")
-		)}
-	</>
+  <>
+    {ReactDOM.createPortal(
+      <Backdrop onClick={onConfirm} />,
+      document.getElementById("backdrop-root")
+    )}
+    {ReactDOM.createPortal(
+      <ModalOverlay title={title} message={message} onConfirm={onConfirm} />,
+      document.getElementById("overlay-root")
+    )}
+  </>
 );
 
 Backdrop.propTypes = {
-	onConfirm: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired,
 };
 
 ModalOverlay.propTypes = {
-	onConfirm: PropTypes.func.isRequired,
-	title: PropTypes.string.isRequired,
-	message: PropTypes.string.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
 };
 
 ErrorModal.propTypes = {
-	title: PropTypes.string,
-	message: PropTypes.string,
-	onConfirm: PropTypes.func.isRequired,
+  title: PropTypes.string,
+  message: PropTypes.string,
+  onConfirm: PropTypes.func.isRequired,
 };
 
 ErrorModal.defaultProps = {
-	title: "No Title Given",
-	message: "No Message Given",
+  title: "No Title Given",
+  message: "No Message Given",
 };
 
 export default ErrorModal;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -6,69 +6,69 @@ import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css";
 
 function AddUser({ onAddUser }) {
-	const nameInputRef = useRef();
-	const ageInputRef = useRef();
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
-	const [error, setError] = useState(false);
+  const [error, setError] = useState(false);
 
-	const addUserHandler = (event) => {
-		event.preventDefault();
-		const enteredName = nameInputRef.current.value;
-		const enteredUserAge = ageInputRef.current.value;
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
 
-		if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
-			setError({
-				title: "Invalid input",
-				message: "Please enter a valid name and age (non-empty values).",
-			});
-			return;
-		}
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
+      return;
+    }
 
-		if (+enteredUserAge < 1) {
-			setError({
-				title: "Invalid age",
-				message: "Please enter a valid age (>0).",
-			});
-			return;
-		}
+    if (+enteredUserAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (>0).",
+      });
+      return;
+    }
 
-		onAddUser(enteredName, enteredUserAge);
-		nameInputRef.current.value = "";
-		ageInputRef.current.value = "";
-	};
+    onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
+  };
 
-	const errorHandler = () => {
-		setError(null);
-	};
+  const errorHandler = () => {
+    setError(null);
+  };
 
-	return (
-		<>
-			{error && (
-				<ErrorModal
-					title={error.title}
-					message={error.message}
-					onConfirm={errorHandler}
-				/>
-			)}
-			<Card className={classes.input}>
-				<form onSubmit={addUserHandler}>
-					<label htmlFor="username">
-						Username
-						<input id="username" type="text" ref={nameInputRef} />
-					</label>
-					<label htmlFor="age">
-						Age (Years)
-						<input type="number" id="age" ref={ageInputRef} />
-					</label>
-					<Button type="submit">Add User</Button>
-				</form>
-			</Card>
-		</>
-	);
+  return (
+    <>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">
+            Username
+            <input id="username" type="text" ref={nameInputRef} />
+          </label>
+          <label htmlFor="age">
+            Age (Years)
+            <input type="number" id="age" ref={ageInputRef} />
+          </label>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </>
+  );
 }
 
 AddUser.propTypes = {
-	onAddUser: PropTypes.func.isRequired,
+  onAddUser: PropTypes.func.isRequired,
 };
 
 export default AddUser;
